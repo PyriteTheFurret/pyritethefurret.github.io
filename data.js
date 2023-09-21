e943b27d290a3c1a988a840726c705b188443bec
PyriteTheFurret <creepersarecool28@gmail.com>
2023-09-21 17:31:15 -0500

    a

--- data.js
+++ data.js
@@ -18,6 +18,66 @@ var data = {
             ]
         }
     ],*/
+    "origin":{
+        "name":[
+            'ORIGO',
+            'ENSIS',
+            'CANTO',
+            'MALUS',
+            'SILEO',
+            'ANTAS',
+            'BREVE',
+            'INANI',
+            'ILLUS',
+            'IGNIS',
+            'GELID',
+            'ARCOS',
+            'PIXUS',
+            'DIVUM',
+            'NORMA',
+            'LODOS',
+            'MONSU',
+            'VICUS',
+            'EVOLU',
+            'MACHI',
+            'FABUL',
+            'STULT',
+            'SPECU',
+            'UMBRA',
+            'PAVOR',
+            'VERUM',
+            'EXIGU'
+        ],
+        "title":[
+            'ORIGO ~ "World of Origin"',
+            'ENSIS ~ "World of the Bladebearer"',
+            'CANTO ~ "World of the Musician"',
+            'MALUS ~ "World of the Manipulated One"',
+            'SILEO ~ "The Reset"',
+            'ANTAS',
+            'BREVE',
+            'INANI ~ "World of the Voidborn"',
+            'ILLUS',
+            'IGNIS ~ "World of the Elementals"',
+            'GELID',
+            'ARCOS ~ "World of the Marksman"',
+            'PIXUS ~ "The Virtual Expanse"',
+            'DIVUM ~ "World of the Space Explorer"',
+            'NORMA',
+            'LODOS',
+            'MONSU ~ "World of the Determined Climber"',
+            'VICUS',
+            'EVOLU',
+            'MACHI ~ "The World, Conquered"',
+            'FABUL ~ "The Storybook"',
+            'STULT',
+            'SPECU ~ "World Beyond the Mirror"',
+            'UMBRA ~ "World in Shadows"',
+            'PAVOR ~ "World of Horrors, Since Collapsed"',
+            'VERUM',
+            'EXIGU'
+        ]
+    },
     /*
         Each character data block has many data values in it:
             "pos": An array of FOUR numbers showing where the character is in the list AND THEIR ORIGIN.
@@ -30,10 +90,13 @@ var data = {
                 I might make different variables for PFP art and other art of the character, but I don't know if I should.
             "gender": Gender.
             "species": Species.
+            "origin": Custom origin name goes here. Leave blank to default to the fourth number in the pos array.
+            "owner": A way to credit anyone else that made the character with you, if you want to. Defaults to the 'owner' tag in the character's main category.
      */
     "data":[
         // 00 - My characters
         {"name":"Pyrite's Characters",
+        "owner":"Pyrite Hue",
         "data":[
             // 00.00 - Pyrite's family
             {"name":"The Family",
@@ -55,7 +118,7 @@ var data = {
                         "pos":[0,0,0,1],
                         "name":"Emerald (Emmy)",
                         "fullname":"Pyrite Emerald Bagels",
-                        "desc":undefined,
+                        "desc":"A kind woman who wields an ornate magic sword. Despite her being referred to as a 'bladebearer' by some, there is no historical or prophetical significance to her blade.",
                         "img":[],
                         "gender":"Female",
                         "species":["Harcolian Delta Furret (ENSIS Timeline Variant)","Maritchian Slime [Variant A]"]
@@ -64,11 +127,11 @@ var data = {
                     {
                         "pos":[0,0,0,2],
                         "name":"Pyra",
-                        "fullname":undefined,
-                        "desc":undefined,
+                        "fullname":"Pyra Hue",
+                        "desc":"A musician with power over Tempo, the element of song and time. Also a pretty decent blacksmith who runs a weapon shop.",
                         "img":[],
                         "gender":"Female",
-                        "species":undefined
+                        "species":["Harcolian Delta Furret","Maritchian Slime [Variant R]"]
                     }
                     // Heliotrope [MALUS]
                     // SILEO Pyrite [SILEO]
@@ -86,13 +149,292 @@ var data = {
                     // {RETIRED} Street Corner Pyrite [VICUS]
                     // {RETIRED} "Andrew", Eevee Pyrite [EVOLU]
                     // {UNKNOWN} Pyrite [MACHI]
-                    // Mica (Paper Pyrite)
-                    // Diorite (Shitpost Pyrite)
-                    // Mirror Harcol Pyrite
+                    // Mica (Paper Pyrite) [FABUL]
+                    // Diorite (Shitpost Pyrite) [STULT]
+                    // Mirror Harcol Pyrite [SPECU]
                     // Lunchmeat [?]
+                ],
+
+                // 00.00.01 - Hazel
+                [
+                    // Hazel [ORIGO]
+                    {
+                        "pos":[0,0,1,0],
+                        "name":"Hazel",
+                        "fullname":"Hazel Marcasite Hue",
+                        "desc":"Certified sentret",
+                        "img":[],
+                        "gender":"Male",
+                        "species":["Delta Sentret","Maritchian Slime [Variant R]"]
+                    },
+                    // Ella (Nutella) [ENSIS]
+                    {
+                        "pos":[0,0,1,1],
+                        "name":"Ella",
+                        "fullname":"Hazel Nutella Bagels",
+                        //"desc":"",
+                        "img":[],
+                        "gender":"Female",
+                        "species":["Harcolian Delta Furret (ENSIS Timeline Variant)"]
+                    },
+                    // Detective Hazel [CANTO]
+                    // Almond/Allie [MALUS]
+                    // SILEO Hazel [SILEO]
+                    // Ellie [LODOS]
                 ]
 /*
+                // 00.00.02 - Amber
+                    Amber [ORIGO]
+                    Calcite [ENSIS]
+                    Amby [SILEO]
 
+/*
+                // 00.00.03 - Golden
+                    Golden
+
+                // 00.00.04 - Cream
+                    Cream
+
+/*
+                // 00.00.05 - Perri
+                    Perri [ORIGO]
+                    Reide [ENSIS]
+
+                // 00.00.06 - Palladium
+                    Palladium [ORIGO]
+                    Palla [ENSIS]
+
+                // 00.00.07 - Wallace
+                    Wallace [ORIGO]
+                    Willow [ENSIS]
+
+                // 00.00.08 - Latte
+                    Latte
+
+                // 00.00.09 - Cappu
+                    Cappu
+
+                // 00.00.10 - Malachite
+                    Malachite
+*/
+
+/*
+                // 00.00.11 - Cobalt
+                    Cobalt [ORIGO]
+                    Aqualite [ENSIS]
+                    Diamond [MALUS]
+                    Chibi Cobalt [ ? ]
+
+                // 00.00.12 - Coffee
+                    Coffee [ORIGO]
+                
+                // 00.00.13 - Copper/Silver
+                    Copper/Silver [ORIGO]
+                    Silvia [SILEO]
+
+/*
+                // 00.00.14 - Topaz
+                    Topaz [ORIGO]
+
+                // 00.00.15 - Orichalcum
+                    Orichalcum [ORIGO]
+
+                // 00.00.16 - Charlotte
+                    Charlotte [ORIGO]
+
+                // 00.00.17 - Crystellia
+                    Crystellia [ORIGO]
+
+                // 00.00.18 - Jade
+                    Jade [ORIGO]
+
+                // 00.00.99 - Unsorted
+                    // Mary [ORIGO]
+                    // Hazel Sr. [ORIGO]
+                    // Jelly [ORIGO]
+                    // Adam [ORIGO]
+                    // Titan [ORIGO]
+                    // Gally [ORIGO]
+                    // Mythril [ORIGO]
+
+
+
+            // 00.01 - Harcolians
+                    // Flare Wick
+                    // ????? (Mother of the Wick Family)
+                    // ????? (Eevee)
+                    // Shayle (Flareon)
+                    // ????? (Jolteon)
+                    // Shel (Vaporeon)
+                    // ????? (Espeon/Umbreon Mix)
+                    // Kel (Leafeon)
+                    // Jem (Glaceon)
+                    // ????? (Sylveon)
+
+                    // Quinn
+                    // Glacoro
+                    // Jay
+                    // Alice (Glacoro/Jay's mother)
+                    // Custard
+                    // Salami
+
+                    // Aquamire Darson
+                    // Kari Darson
+                    // Lilly Valley
+                    // Milly Valley
+                    // Antonio (Anthro Drifloon)
+                    // Chili the Skitty
+
+                    // Fenn
+                    // Carol (Fenn's mother)
+                    // Ferlehu
+
+                    // Dr. Pikachu
+                    // Prof. Eevee
+                    // Gilly
+
+                    // Duke Ex Magearna [M1/M2]
+                    // Alex
+                    // Jackal (Bounty Hunter)
+
+                    // Jim (Reshiram)
+                    // ????? (Zekrom)
+                    // Kunai (Articuno)
+                    // ????? (Zapdos Businesswoman)
+                    // Mace (Moltres Businessman)
+
+                // 00.01.99 - Unsorted
+                    // ????? (Neutral Type Arceus)
+                    // ????? (Fire Type Arceus)
+                    // ????? (Water Type Arceus)
+                    // ????? (Grass Type Arceus)
+                    // ????? (Electric Type Arceus)
+                    // ????? (Ice Type Arceus)
+                    // ????? (Fighting Type Arceus)
+                    // Arkaeya The Psychic
+                    // ????? (Poison Type Arceus)
+                    // ????? (Ground Type Arceus)
+                    // ????? (Rock Type Arceus)
+                    // ????? (Flying Type Arceus)
+                    // ????? (Bug Type Arceus)
+                    // ????? (Ghost Type Arceus)
+                    // ????? (Dragon Type Arceus)
+                    // ????? (Dark Type Arceus)
+                    // ????? (Steel Type Arceus)
+                    // ????? (Fairy Type Arceus)
+
+            // 00. - Jasper Arc OCs
+                // 00. .99 - Unsorted
+                    Dot
+                    Chungy (Dot's Furret)
+                    Jasper
+                    ????? (Dot's mother)
+                    Professor Acacia
+                    Fel (Experiment Zero)
+                    Pitch
+                    Terror
+                    The Third Experiment [BYPASS]
+                    Malice
+                    Jelly/Shift/Ranger
+                    Pseudobane
+                    Zenith
+
+            // 00. - Post-Harcol, Pre-TGS era OCs
+                // 00. .99 - Unsorted
+                    Dell
+                    Vulcan
+                    Darkrai
+                    Kecleon
+                    Pyrute
+                    The Evil Cyndaquil
+                    Griffin Von Byrd
+                    Chad
+                    Billingsworth Cash
+                    Robert (James)
+
+            // 00. - Fragments and other Ethereal-Adjacent OCs
+                // 00. .99 - Unsorted
+                    The Fragment of Sound
+                    Craig (The Fragment of Death)
+                    The Fragment of Life
+                    The Fragment of Light
+                    The Fragment of Mind
+                    Chroma (The Fragment of Color)
+                    Galaxy (The Fragment of Space)
+                    The Fragment of Temperature
+                    Joules (The Fragment of Energy)
+                    The Fragment of Tempo
+
+                    Nebula (Cobalt's Oneiroi)
+                    Aqua (Reborn Cobalt's Guardian Voice)
+                    Talon (Reborn Coffee's Guardian Voice)
+
+            // 00. - Rain World OCs
+                // 00. .99 - Unsorted
+                    Deceptive Glowing Stones (+ Explorer)
+                    Heart of Metal
+                    The Skield
+                    The Augmented
+
+                    Survivor woman
+                    Monk woman
+                    Hunter woman
+                    Gourmand woman
+                    Artificer woman
+                    Spearmaster woman
+                    Rivulet woman
+                    Saint woman
+
+            // 00. - Other Game OCs
+                // 00. .99 - Unsorted
+                    Reuben (Terraria idiot)
+                    Felos (Terraria | The Desert Ranger)
+                    Beryl (Terraria | Mage)
+                    Green Lemon (Terraria | Mage)
+                    Talos, the Stealth Striker (Terraria)
+                    Xen, Druid of Terralon / Xen, the Accursed
+
+                    Kane (Minecraft OC)
+
+                    Zote Calico
+                    Erin Calico
+                    Vin Ven
+                    Zote Zeneki
+
+                    Ellis the Enforcer
+                    ????? (Anthro Lionfish)
+
+            // 00. - 
+                // 00. .99 - Unsorted
+
+            // 00. - 
+                // 00. .99 - Unsorted
+
+            // 00. - Other
+                // 00. .99 - Unsorted
+                    Peter (Gem Chronicles)
+                    Jym (Gem Chronicles)
+                    IRL IRP Pyrite
+                    Chibi Peter
+                    Mendel the Menderbug
+                    The French Duck
+                    The Soulwalkers
+                    The Friendly Soulwalkers
+
+            
+            // 00.99 - Unsorted
+                    Rogue (A Helio Bot, broken out and rebelling)
+                    Addie, Core of Corruption
+                    Radium
+                    Mellow
+                    Unknown (The Cabinet Man)
+                    Harmony [RETCONNED]
+                    Mini (Tree)
+                    Solstice (Boudinian, Forever Changed)
+                    Gank Von Campe (Unknown Sileo Character)
+                    Kaze (Unknown Sileo Character)
+                    Jared, the Chaotic One (Squirrel)
+                    Nekonecny (The Fractal Man)
 */
             ]}
         ]}
--- databaseScripts.js
+++ databaseScripts.js
@@ -1,81 +1,106 @@
-var selCat = document.getElementById("selCat");
 var curOpt = [0,0,0,0];
+
+// This is here so that the jQuery code doesn't run until after the document is ready.
+// Update: it just makes my code not work at all, at least locally. Great.
+//$(function(){
 
 function reselect(selectId,changedVal) {
     try {
         if (changedVal != undefined) {
             curOpt[selectId] = changedVal;
         }
-        //Switch for the different category types
+        // Switch for the different category types
         switch (selectId) {
             // 0 is the main category
             // Populate the first subcategory
             case 0:
-                var deathRow = document.getElementsByClassName("subOpt1");
-                deathRow.forEach(element => {
-                    element.remove();
-                });
+                $("#selSub1").empty();
+
                 for (var i = 0; i < data["data"][curOpt[0]]["data"].length; i++) {
+                    // Create an option element for each... well, each option.
+                    // 
                     var subOpt1 = document.createElement("option");
-                    //The joke is that this entire script is very likely suboptimal! :)
-                    subOpt1.text = data["data"][curOpt[0]]["data"][i]["name"];
+                    subOpt1.text = (data["data"][curOpt[0]]["data"][i]["name"]);
                     subOpt1.value = i;
-                    subOpt1.classList = "subOpt1";
-                    document.getElementById("selSub1").appendChild(subOpt1);
+                    $("#selSub1").append(subOpt1);
                 }
                 reselect(1,0);
                 break;
             // 1 is the first subcategory
             // Populate the character subcategory
             case 1:
-                var deathRow = document.getElementsByClassName("subOpt2");
-                deathRow.forEach(element => {
-                    element.remove();
-                });
+                $("#selSub2").empty();
+
                 for (i = 0; i < data["data"][curOpt[0]]["data"][curOpt[1]]["data"].length; i++) {
                     var subOpt2 = document.createElement("option");
-                    subOpt2.text = data["data"][curOpt[0]]["data"][curOpt[1]]["data"][i][0]["name"];
+                    subOpt2.text = (data["data"][curOpt[0]]["data"][curOpt[1]]["data"][i][0]["name"]);
                     subOpt2.value = i;
-                    subOpt2.classList = "subOpt2";
-                    document.getElementById("selSub2").appendChild(subOpt2);
+                    $("#selSub2").append(subOpt2);
                 }
                 reselect(2,0);
                 break;
             // 2 is the character subcategory
             // Populate the origin subcategory
             case 2:
-                var deathRow = document.getElementsByClassName("origOpt");
-                deathRow.forEach(element => {
-                    element.remove();
-                });
+                $("#selOrig").empty();
+                /* TODO: Add a way to handle characters with unspecified/custom origins */
                 if (data["data"][curOpt[0]]["data"][curOpt[1]]["data"][curOpt[2]].length > 1) {
+                    $("#selOrig").css("display","inline");
                     for (i = 0; i < data["data"][curOpt[0]]["data"][curOpt[1]]["data"][curOpt[2]].length; i++) {
                         var origOpt = document.createElement("option");
-                        origOpt.text = data["data"][curOpt[0]]["data"][curOpt[1]]["data"][curOpt[2]][i]["pos"][3];
+                        if (data["data"][curOpt[0]]["data"][curOpt[1]]["data"][curOpt[2]][i]["origin"] != undefined) {
+                            origOpt.text = data["data"][curOpt[0]]["data"][curOpt[1]]["data"][curOpt[2]][i]["origin"];
+                        } else origOpt.text = data["origin"]["name"][data["data"][curOpt[0]]["data"][curOpt[1]]["data"][curOpt[2]][i]["pos"][3]];
                         origOpt.value = i;
-                        origOpt.classList = "origOpt";
-                        document.getElementById("selOrig").appendChild(origOpt);
+                        $("#selOrig").append(origOpt);
                     }
+                } else {
+                    $("#selOrig").css("display","none");
                 }
                 reselect(3,0);
                 break;
-            //3 is the origin subcategory
+            // 3 is the origin subcategory
+            // Fetch the data of the character
             case 3:
-                var list = ["name","fullname","gender","desc"];
+                // Generic attributes (if it's there, put it there; if it's not, hide it)
+                var list = ["name","fullname","gender","desc"]; // List of generic attributes
                 for (var i = 0; i < list.length; i++) {
                     if (data["data"][curOpt[0]]["data"][curOpt[1]]["data"][curOpt[2]][curOpt[3]][list[i]] != undefined) {
-                        document.getElementById("span" + list[i]).style.display = "inline";
-                        document.getElementById(list[i]).innerText = data["data"][curOpt[0]]["data"][curOpt[1]]["data"][curOpt[2]][curOpt[3]][list[i]];
-                    } else document.getElementById("span" + list[i]).style.display = "none";
+                        $("#span" + list[i]).css("display","inline");
+                        $("#" + list[i]).text(data["data"][curOpt[0]]["data"][curOpt[1]]["data"][curOpt[2]][curOpt[3]][list[i]]);
+                    } else $("#span" + list[i]).css("display","none");
                 }
-                //Special cases (Species, origin)
+
+                // Special cases
+
+                // Species
                 if (data["data"][curOpt[0]]["data"][curOpt[1]]["data"][curOpt[2]][curOpt[3]]["species"] != undefined) {
-                    document.getElementById("spanspecies").style.display = "inline";
-                    document.getElementById("species").innerText = data["data"][curOpt[0]]["data"][curOpt[1]]["data"][curOpt[2]][curOpt[3]]["species"].join(", ");
-                } else document.getElementById("spanspecies").style.display = "none";
+                    $("#spanspecies").css("display","inline");
+                    $("#species").text(data["data"][curOpt[0]]["data"][curOpt[1]]["data"][curOpt[2]][curOpt[3]]["species"].join(", "));
+                } else $("#spanspecies").css("display","none");
+                
+                // Origin
+                if (data["data"][curOpt[0]]["data"][curOpt[1]]["data"][curOpt[2]][curOpt[3]]["origin"] != undefined) {
+                    // if origin is defined
+                    $("#spanorigin").css("display","inline");
+                    if (isNaN(data["data"][curOpt[0]]["data"][curOpt[1]]["data"][curOpt[2]][curOpt[3]]["origin"])) {
+                        // if origin is not a number (custom origin name)
+                        $("#origin").text(data["data"][curOpt[0]]["data"][curOpt[1]]["data"][curOpt[2]][curOpt[3]]["origin"]);
+                    }
+                    $("#origin").text(data["data"][curOpt[0]]["data"][curOpt[1]]["data"][curOpt[2]][curOpt[3]]["origin"]);
+                } else if (data["data"][curOpt[0]]["data"][curOpt[1]]["data"][curOpt[2]][curOpt[3]]["pos"][3] != undefined) {
+                    //if origin is not defined, but pos has a fourth number in it (number origin)
+                    $("#spanorigin").css("display","inline");
+                    $("#origin").text(data["origin"]["title"][data["data"][curOpt[0]]["data"][curOpt[1]]["data"][curOpt[2]][curOpt[3]]["pos"][3]]);
+                } else $("#spanorigin").css("display","none");
+                
+                //Owner
+                if (data["data"][curOpt[0]]["data"][curOpt[1]]["data"][curOpt[2]][curOpt[3]]["owner"] != undefined) {
+                    $("#owner").text(data["data"][curOpt[0]]["data"][curOpt[1]]["data"][curOpt[2]][curOpt[3]]["owner"]);
+                } else $("#owner").text(data["data"][curOpt[0]]["owner"]);
                 break;
             default:
-                //what the fuck did you do to get here, how
+                // what the fuck did you do to get here, how
         }
     }
     catch (err) {
@@ -85,41 +110,20 @@ function reselect(selectId,changedVal) {
 
 function initStuff() {
     try {
-        //Add options to the select tags
-        //for loop to add stuff
+        // Add options to the select tags
+        // for loop to add stuff
         for (var i = 0; i < data["data"].length; i++) {
-            //Categories
+            // Categories
             var catOpt = document.createElement("option");
             catOpt.text = data["data"][i]["name"];
             catOpt.value = i;
-            selCat.appendChild(catOpt);
+            $("#selCat").append(catOpt);
         }
-        for (var i = 0; i < data["data"][curOpt[0]]["data"].length; i++) {
-            var subOpt1 = document.createElement("option");
-            subOpt1.text = data["data"][curOpt[0]]["data"][i]["name"];
-            subOpt1.value = i;
-            subOpt1.classList = "subOpt1";
-            document.getElementById("selSub1").appendChild(subOpt1);
-        }
-        for (i = 0; i < data["data"][curOpt[0]]["data"][curOpt[1]]["data"].length; i++) {
-            var subOpt2 = document.createElement("option");
-            subOpt2.text = data["data"][curOpt[0]]["data"][curOpt[1]]["data"][i][0]["name"];
-            subOpt2.value = i;
-            subOpt2.classList = "subOpt2";
-            document.getElementById("selSub2").appendChild(subOpt2);
-        }
-        for (i = 0; i < data["data"][curOpt[0]]["data"][curOpt[1]]["data"][curOpt[2]].length; i++) {
-            var origOpt = document.createElement("option");
-            origOpt.text = data["data"][curOpt[0]]["data"][curOpt[1]]["data"][curOpt[2]][i]["pos"][3];
-            origOpt.value = data["data"][curOpt[0]]["data"][curOpt[1]]["data"][curOpt[2]][i]["pos"][3];
-            origOpt.classList = "origOpt";
-            document.getElementById("selOrig").appendChild(origOpt);
-        }
-        reselect(3);
+        reselect(0);
     }
     catch (err) {
         alert("You done goofed! (" + err + ")");
     }
 }
-
+//});
 initStuff();
